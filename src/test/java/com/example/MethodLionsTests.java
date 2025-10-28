c07f3084c90d741c2591e23d696d7fbce1263ec2
Irina Korneva <kornevairka1525@yandex.ru>
2025-10-28 14:57:40 +0500

    Изменила аннотацию мока и метод

--- src/test/java/com/example/MethodLionsTests.java
+++ src/test/java/com/example/MethodLionsTests.java
@@ -15,24 +15,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MethodLionsTests {
 
     @Mock
-    Predator predator;
+    Feline feline;
 
     @Test
     public void getFoodAsPredator() throws Exception {
-        Mockito.when(predator.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        Lion lion = new Lion("Самец", predator);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        Lion lion = new Lion("Самец", feline);
         assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood(), "Самцы едят: 'Животные', 'Птицы', 'Рыба'");
     }
 
     @Test
     public void getKittensAsPredator() throws Exception {
-        Mockito.when(predator.getKittens()).thenReturn(3);
-        Lion lion = new Lion("Самка", predator);
+        Mockito.when(feline.getKittens()).thenReturn(3);
+        Lion lion = new Lion("Самка", feline);
         assertEquals(3, lion.getKittens(), "У самки должно быть 3 львенка");
     }
 
     @Test
     public void negativeSex() {
-        assertEquals("Используйте допустимые значения пола животного - самей или самка", assertThrows(Exception.class, () -> new Lion("Другое", predator)).getMessage());
+        assertEquals("Используйте допустимые значения пола животного - самей или самка", assertThrows(Exception.class, () -> new Lion("Другое", feline)).getMessage());
     }
 }
